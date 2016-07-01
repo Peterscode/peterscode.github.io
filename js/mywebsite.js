43bbb646ea93043bae6873f383ca557891171277
Peterscode <duongxpeter@gmail.com>
2016-07-01 14:35:07 -0400

    Add files via upload

---
+++ js/mywebsite.js
@@ -0,0 +1,4 @@
+//this functions makes the nav bar (top nav) get triggered on the menu icon, making it responsive
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
